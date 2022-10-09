a7a7ec96825ba24f4b37d1059b41e88d75ad194a
Oleraj <olerajhossin@gmail.com>
2022-10-09 12:23:34 +0600

    Without responsive

--- assests/js/function.js
+++ assests/js/function.js
@@ -8,3 +8,52 @@ $(function(){
     }
   });
 });
+
+//Testimonial Data
+const testimonials = [
+  {
+    name: "Eva Sawyer",
+    job: "CEO, Fashworks",
+    image: "../media/images/test1.PNG",
+    testimonial:
+      "Neque volutpat ac tincidunt vitae semper quis lectus nulla at volutpat diam ut venenatis tellus in metus vulputate eu scelerisque felis imperdiet proin fermentum leo vel orci porta non pulvinar neque laoreet suspendisse interdum consectetur",
+  },
+  {
+    name: "Katey Topaz",
+    job: "Developer, TechCrew",
+    image: "../media/images/test2.PNG",
+    testimonial:
+      "Elementum tempus egestas sed sed risus pretium quam vulputate dignissim suspendisse in est ante in nibh mauris cursus mattis molestie a iaculis at erat pellentesque adipiscing commodo elit at imperdiet dui accumsan sit amet nulla",
+  },
+  {
+    name: "Jae Robin",
+    job: "UI Designer, Affinity Agency",
+    image: "../media/images/test3.PNG",
+    testimonial:
+      "Orci nulla pellentesque dignissim enim sit amet venenatis urna cursus eget nunc scelerisque viverra mauris in aliquam sem fringilla ut morbi tincidunt augue interdum velit euismod in pellentesque massa placerat duis ultricies lacus sed turpis",
+  },
+];
+//Current Slide
+let i = 0;
+//Total Slides
+let j = testimonials.length;
+let testimonialContainer = document.getElementById("testimonial-container");
+let nextBtn = document.getElementById("next");
+let prevBtn = document.getElementById("prev");
+nextBtn.addEventListener("click", () => {
+  i = (j + i + 1) % j;
+  displayTestimonial();
+});
+prevBtn.addEventListener("click", () => {
+  i = (j + i - 1) % j;
+  displayTestimonial();
+});
+let displayTestimonial = () => {
+  testimonialContainer.innerHTML = `
+    <p>${testimonials[i].testimonial}</p>
+    <img src=${testimonials[i].image}>
+    <h3>${testimonials[i].name}</h3>
+    <h6>${testimonials[i].job}</h6>
+  `;
+};
+window.onload = displayTestimonial;
